c71957973af641babdc2b4deafb861c9413f4af7
Huaxin Zhang <66454973+pipixin321@users.noreply.github.com>
2024-01-05 20:49:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-# python main.py --cfg thumos --stage 1 --mode train
-# python main.py --cfg thumos --stage 1 --mode test
-# python main.py --cfg thumos --stage 2 --mode train
-# python main.py --cfg thumos --stage 2 --mode test
-python main.py --cfg thumos --stage 1 --mode train --seed 6
-python main.py --cfg thumos --stage 1 --mode test  --seed 6
-python main.py --cfg thumos --stage 2 --mode train  --seed 0
-python main.py --cfg thumos --stage 2 --mode test   --seed 0
+python main.py --cfg thumos --stage 1 --mode train
+python main.py --cfg thumos --stage 1 --mode test
+python main.py --cfg thumos --stage 2 --mode train
+python main.py --cfg thumos --stage 2 --mode test
